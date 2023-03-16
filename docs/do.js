27cdc3dc0d6a20685e5bf9188fe22dd2a65730a0
gndpwnd <gnelsondev@gmail.com>
2023-03-15 22:24:54 -0800

    update search engines

--- docs/do.js
+++ docs/do.js
@@ -4,39 +4,48 @@ disclaimer.style.display = "none";
 
 // Define arrays of search engines for each filter
 const webSearchEngines = [
-  "www.google.com/search?q=", 
-  "www.bing.com/search?q=", 
-  "search.yahoo.com/search?p="
+  "www.google.com/search?q=",
+  "searx.tiekoetter.com/search?q=",  
+  "search.yahoo.com/search?p=",
+  "yandex.com/search/?text=",
+  "www.seekr.com/search?query=",
+  "www.mojeek.com/search?q=",
+  "duckduckgo.com/?q=",
+  "www.bing.com/search?q=",
 ];
 
 const scholarSearchEngines = [
-  "Google Scholar", 
-  "Microsoft Academic", 
-  "Semantic Scholar"
-];
-
-const newsSearchEngines = [
-  "CNN", 
-  "BBC", 
-  "New York Times"
+  "scholar.google.com/scholar?hl=en&as_sdt=0%2C2&q=", 
+  "www.jstor.org/action/doBasicSearch?Query=",
+  "www.semanticscholar.org/search?sort=relevance&q=",
+  "www.refseek.com/search?q=",
+  "core.ac.uk/search?q=",
+  "eric.ed.gov/?q=",
+  "www.base-search.net/Search/Results?oaboost=1&newsearch=1&refid=dcbasen&lookfor=",
+  "www.mendeley.com/search/?query=",
+  "www.jurn.link/#gsc.tab=0&gsc.q=",
+  "researchworks.oclc.org/archivegrid/?q=",
+  "paperity.org/search/?q=",
+  "fatcat.wiki/release/search?q=",
+  "www.econbiz.de/Search/Results?type=AllFields&lookfor=",
+  "www.worldcat.org/search?q=",
 ];
 
 const databaseSearchEngines = [
-  "PubMed", 
-  "JSTOR", 
-  "IEEE Xplore"
+  "archive.org/search?query=", 
+  "datasetsearch.research.google.com/search?query=", 
+  "catalog.data.gov/dataset?q=",
+  "www.elephind.com/?a=q&hs=1&r=1&results=1&txq="
 ];
 
 // Get the filter checkboxes and search button
 const webFilter = document.getElementById("web-filter");
 const scholarFilter = document.getElementById("scholar-filter");
-const newsFilter = document.getElementById("news-filter");
 const databaseFilter = document.getElementById("database-filter");
 const searchBtn = document.getElementById('search-btn');
 
 let web_urls = [];
 let scholar_urls = [];
-let news_urls = [];
 let database_urls = [];
 let browserName;
 
@@ -44,6 +53,16 @@ function disco(){
   // for every url list, if the corresponding filter has been checked, open up new tabs for each url
   if (webFilter.checked) {
     for (const element of web_urls) {
+      window.open("https://"+element, "_blank");
+    }
+  }
+  if (scholarFilter.checked) {
+    for (const element of scholar_urls) {
+      window.open("https://"+element, "_blank");
+    }
+  }
+  if (databaseFilter.checked) {
+    for (const element of database_urls) {
       window.open("https://"+element, "_blank");
     }
   }
@@ -56,11 +75,11 @@ function discoveryConduct(){
   // clear urls out
   web_urls = [];
   scholar_urls = [];
-  news_urls = [];
   database_urls = [];
 
-  const searchQuery = document.getElementById("search-input").value;
-  //console.log(searchQuery);
+  let searchQuery = document.getElementById("search-input").value;
+  // replace spaces with plus signs
+  searchQuery = searchQuery.replace(/ /g, "+");
 
   // if search query is blank, show disclaimer and exit
   if (searchQuery == "") {
@@ -77,11 +96,6 @@ function discoveryConduct(){
   for (const element of scholarSearchEngines) {
     let url = element + searchQuery;
     scholar_urls.push(url);
-  }
-
-  for (const element of newsSearchEngines) {
-    let url = element + searchQuery;
-    news_urls.push(url);
   }
 
   for (const element of databaseSearchEngines) {
